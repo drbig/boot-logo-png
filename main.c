7342661c02c69004cf118298607f7e3a12980d0b
Piotr S. Staszewski <p.staszewski@gmail.com>
2020-07-03 23:11:33 +0200

    And this version works fine

--- main.c
+++ main.c
@@ -1,39 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include <SDL.h>
 #include <SDL/SDL_image.h>
 
-int main(int argc, char **argv) {
-  SDL_Surface *img_ready;
-  int retVal = SDL_Init(SDL_INIT_VIDEO);
-  if (retVal < 0) {
-    printf("SDL_Init! SDL_Error: %s\n", SDL_GetError());
-  } else {
-    SDL_Surface *screen = SDL_SetVideoMode(320, 240, 16, SDL_SWSURFACE);
-    if (screen == NULL) {
-      printf("SDL_SetVideoMode! SDL_Error: %s\n", SDL_GetError());
-    } else {
-      SDL_Surface *img = IMG_Load("/mnt/drbig/image-16-rgb565.bmp");
-      if (img == NULL) {
-        printf("IMG_Load! SDL_Error: %s\n", SDL_GetError());
-      } else {
-        img_ready = SDL_ConvertSurface(img, screen->format, SDL_SWSURFACE);
-        if (img_ready == NULL) {
-          printf("SDL_ConvertSurface! SDL_Error: %s\n", SDL_GetError());
-        } else {
-          printf("Ready to SDL_BlitSurface!\n");
-          retVal = SDL_BlitSurface(img_ready, NULL, screen, NULL);
-          if (retVal < 0) {
-            printf("SDL_BlitSurface! SDL_Error: %s\n", SDL_GetError());
-          } else {
-            printf("Done SDL_BlitSurface!\n");
-          }
-          SDL_Delay(5000);
-        }
-        SDL_FreeSurface(img);
-      }
-    }
+int main(int argc, char *argv[]) {
+  SDL_Surface *logo_img;
+  int sleep_ms = 0;
+
+  if (argc != 3) {
+    fprintf(stderr, "Usage: %s <sleep_ms> <image.png>\n", argv[0]);
+    return 1;
   }
+  if ((sleep_ms = atoi(argv[1])) < 300) {
+    fprintf(stderr, "Sleep_ms lower than 300 ms: %s\n", sleep_ms);
+    return 1;
+  }
+  if ((logo_img = IMG_Load(argv[2])) == NULL) {
+    fprintf(stderr, "Unable to load logo: %s\n", SDL_GetError());
+    return 1;
+  }
+
+  if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+    fprintf(stderr, "Unable to initialize SDL: %s\n", SDL_GetError());
+    return 2;
+  }
+  SDL_Surface *screen = SDL_SetVideoMode(320, 240, 16, SDL_SWSURFACE);
+  SDL_ShowCursor(SDL_DISABLE);
+  SDL_BlitSurface(logo_img, NULL, screen, NULL);
+  SDL_Flip(screen);
+  SDL_Delay(sleep_ms);
   SDL_Quit();
-  return retVal;
+
+  return 0;
 }
